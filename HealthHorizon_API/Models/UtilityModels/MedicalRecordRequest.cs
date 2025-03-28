90ece0d055cfe5d0ac33c17ed0b197f7d9a85925
BrentNBaillie <151245998+BrentNBaillie@users.noreply.github.com>
2025-03-27 21:05:44 -0400

    Delete HealthHorizon_API directory

--- HealthHorizon_API/Models/UtilityModels/MedicalRecordRequest.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace HealthHorizon_API.Models.UtilityModels
-{
-	public class MedicalRecordRequest
-	{
-		public int Id { get; set; }
-		public string Categories { get; set; } = string.Empty;
-	}
-}
